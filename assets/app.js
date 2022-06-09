bcb027fb83489aedd9551eb2d5fc871ba2c32949
Honoré Morel <morelhonore@gmail.com>
2022-06-09 14:44:58 +0200

    Début projet

--- assets/app.js
+++ assets/app.js
@@ -8,5 +8,22 @@
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.css';
 
-// start the Stimulus application
-import './bootstrap';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () =>{
+
+
+    return(
+        <>
+            <input type="text"/>
+        </>
+    )
+}
+
+
+
+
+
+
+ReactDOM.render(<App/>, document.getElementById("root"));
